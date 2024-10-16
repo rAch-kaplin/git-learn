97f7073fd95f49c5c26e20ebc5c5aa53df5a9b9e
rAch-kaplin <kaplinstu1@gmail.com>
2024-10-16 16:27:36 +0300

    success with fprintf and assert

--- sort.cpp
+++ sort.cpp
@@ -2,35 +2,15 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 int my_strcmp(const char *pstr1, const char *pstr2);
 void bubbleSort(char **str, size_t size);
 
 int main()
 {
-
-    // const char* str[] = {"My uncle, man of firm convictions*…",
-    //                     "By falling gravely ill, he's won",
-    //                     "A due respect for his afflictions-",
-    //                     "The only clever thing he's done.",
-    //                     "May his example profit others;",
-    //                     "But God, what deadly boredom, brothers,",
-    //                     "To tend a sick man night and day,",
-    //                     "Not daring once to steal away!",
-    //                     "And, oh, how base to pamper grossly",
-    //                     "And entertain the nearly dead,",
-    //                     "To fluff the pillows for his head,",
-    //                     "And pass him medicines morosely-",
-    //                     "While thinking under every sigh:",
-    //                     "The devil take you, Uncle. Die!"};
-    // const size_t SIZE = sizeof(str) / sizeof(str[0]);
-    //printf("<<size = %zu>>\n", SIZE);
     FILE * file = fopen("onegin.txt", "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Ошибка открытия файла\n");
-        return 1;
-    }
+    assert(file != NULL);
 
     const size_t MAX_LENGHT_STRING = 100;
     char buffer[MAX_LENGHT_STRING] = {};
@@ -38,6 +18,7 @@ int main()
     size_t row_count = 10;
 
     char **str = (char**)calloc(row_count, sizeof(char*));
+    assert(str != NULL);
 
     while (fgets(buffer, MAX_LENGHT_STRING, file) != NULL)
     {
@@ -46,9 +27,10 @@ int main()
         if (len > 0 && buffer[len - 1] == '\n')
             buffer[len - 1] = '\0';
 
-        printf("Прочитанная строка: %s\n", buffer);
+        //printf("Прочитанная строка: %s\n", buffer);
 
         str[SIZE] = (char*)calloc((len + 1), sizeof(char));
+        assert(str != NULL);
         strcpy(str[SIZE], buffer);
         SIZE++;
 
@@ -56,22 +38,34 @@ int main()
         {
             row_count *= 2;
             str = (char**)realloc(str, row_count * sizeof(char*));
+            assert(str != NULL);
         }
     }
     fclose(file);
 
-    printf("<<%zu>>\n", SIZE);
+    printf("SIZE = <<%zu>>\n\n", SIZE);
 
+    puts("Содержимое считанного файла:\n");
     for (size_t j = 0; j < SIZE; j++)
     {
-        printf("---%s \n", str[j]);
+        printf("---%s\n", str[j]);
     }
     putchar('\n');
 
     bubbleSort(str, SIZE);
+
+    FILE *fp = fopen("onegin_sorted.txt", "w");
+    assert(fp != NULL);
     for (size_t j = 0; j < SIZE; j++)
     {
-        printf(" %s \n", str[j]);
+        fprintf(fp, "%s\n", str[j]);
+    }
+    fclose(fp);
+
+    puts("Отсортированный текст:\n");
+    for (size_t j = 0; j < SIZE; j++)
+    {
+        printf("%s\n", str[j]);
         free(str[j]);
     }
     free(str);
@@ -102,16 +96,13 @@ int my_strcmp(const char *pstr1, const char *pstr2)
             return ch1 - ch2;
     }
 }
+
 void bubbleSort(char **str, size_t SIZE)
 {
     for (size_t i = 0; i < SIZE - 1; i++)
     {
         for (size_t j = 0; j < SIZE - i - 1; j++)
         {
-            if (str[j] == NULL || str[j + 1] == NULL)
-            {
-                continue;
-            }
             int res = my_strcmp(str[j], str[j + 1]);
             if (res > 0)
             {
