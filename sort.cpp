9b62bdac46ffd1053455f7d91ed3ef70345e2b48
rAch-kaplin <kaplinstu1@gmail.com>
2024-10-16 22:02:52 +0300

    add readingLines in function / working

--- sort.cpp
+++ sort.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <assert.h>
 
+void readingLines(char ***str, size_t *SIZE, size_t *row_count, FILE *file);
 int my_strcmp(const char *pstr1, const char *pstr2);
 void bubbleSort(char **str, size_t size);
 
@@ -12,14 +13,14 @@ int main()
     FILE * file = fopen("onegin.txt", "r");
     assert(file != NULL);
 
-    const size_t MAX_LENGHT_STRING = 100;
-    char buffer[MAX_LENGHT_STRING] = {};
     size_t SIZE = 0;
     size_t row_count = 10;
 
     char **str = (char**)calloc(row_count, sizeof(char*));
     assert(str != NULL);
 
+    readingLines(&str, &SIZE, &row_count, file);
+#if 0
     while (fgets(buffer, MAX_LENGHT_STRING, file) != NULL)
     {
         size_t len = strlen(buffer);
@@ -41,6 +42,8 @@ int main()
             assert(str != NULL);
         }
     }
+#endif
+
     fclose(file);
 
     printf("SIZE = <<%zu>>\n\n", SIZE);
@@ -70,6 +73,33 @@ int main()
     }
     free(str);
     return 0;
+}
+
+void readingLines(char ***str, size_t *SIZE, size_t *row_count, FILE *file)
+{
+    const size_t MAX_LENGHT_STRING = 100;
+    char buffer[MAX_LENGHT_STRING] = {};
+
+    while (fgets(buffer, MAX_LENGHT_STRING, file) != NULL)
+    {
+        size_t len = strlen(buffer);
+        //printf("strlen(buffer = %zu)\n", len);
+        if (len > 0 && buffer[len - 1] == '\n')
+            buffer[len - 1] = '\0';
+
+        //printf("Прочитанная строка: %s\n", buffer);
+
+        (*str)[*SIZE] = (char*)calloc((len + 1), sizeof(char));
+        assert((*str) != NULL);
+        strcpy((*str)[*SIZE], buffer);
+        (*SIZE)++;
+        if (*SIZE >= *row_count)
+        {
+            *row_count *= 2;
+            *str = (char**)realloc(*str, *row_count * sizeof(char*));
+            assert(*str != NULL);
+        }
+    }
 }
 
 int my_strcmp(const char *pstr1, const char *pstr2)
